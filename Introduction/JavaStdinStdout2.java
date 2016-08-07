fa9fd567b1518114af8bd9bc4068808961a0328d
keshav143420 <keshav143420@gmail.com>
2016-08-07 03:44:50 +0000

    Welcome to java, Stdin and Stdout problems

---
+++ Introduction/JavaStdinStdout2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int x = sc.nextInt();
+            double y = sc.nextDouble();
+            sc.nextLine();
+            String s2 = sc.nextLine();
+            System.out.println("String: " + s2);
+            System.out.println("Double: " + y);
+            System.out.println("Int: " + x);
+    }
+}
